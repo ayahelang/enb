aec95e53c10e7fe73ee60f2a530d7a046b3e2e76
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-03-16 14:20:05 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -52,3 +52,19 @@ async function loadExperiments() {
 }
 
 loadExperiments()
+
+function toggleMenu() {
+
+    const nav = document.getElementById("navMenu")
+
+    if (nav.style.display === "flex") {
+
+        nav.style.display = "none"
+
+    } else {
+
+        nav.style.display = "flex"
+
+    }
+
+}
